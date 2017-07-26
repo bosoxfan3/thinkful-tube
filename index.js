eb2b0b320c718024c8b19611ceef6a4a474bac4a
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-07-26 11:26:16 -0700

    wrote the displayYouTubeSearchData function and removed the placeholders

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
+'use strict';
+
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 function getDataFromApi(searchTerm, callback) {
-    const query = {
-        q: `${searchTerm}`,
-        per_page: 10,
-        part: `snippet`,
-        key: 'AIzaSyBQV-GhhCOVYxkTVYtSzufauAvpVxNr_4o',      
-    }
-    $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+  const query = {
+    q: `${searchTerm}`,
+    per_page: 10,
+    part: 'snippet',
+    key: 'AIzaSyBQV-GhhCOVYxkTVYtSzufauAvpVxNr_4o',      
+  };
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
-// function renderResult(result) {
-//     return  `
-//     <div>
-//     <a href="`youtu.be/${result.items[i].id.videoId}`" ><img src="`${result.items[i].snippet.thumbnails.default.url}`"> </a>
-//     <h3> </h3>
-//     <p> </p>
-//     </div> 
-//     `;
-// }
+function renderResult() {
+  return  `
+    <div>
+    <a href=""><img src=""></a>
+    <h3></h3>
+    <p></p>
+    </div> 
+    `;
+}
+
+
+
+function displayYouTubeSearchData(data) {
+  const results = data.items.map((item, index) =>
+    renderResult(item));
+  $('.js-search-results').html(results);
+}
 
 $('.the-one').click(function(event) {
-    event.preventDefault();
-    //renderResult();
-    console.log('am i broken here?');
+  event.preventDefault();
+  renderResult();
+  console.log('am i broken here?');
 });
